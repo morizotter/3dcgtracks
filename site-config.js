93146b88941ed185eebec45abc0a8d6d45dbde25
Naoki Morita <namorit@gmail.com>
2019-01-16 01:59:43 +0900

    Initial commit

---
+++ site-config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  siteTitle: `3DCG Tracks`,
+  siteTitleShort: `Tracks`,
+  siteDescription: `The tracks of 3dcg practices.`,
+  siteUrl: `https://moritanaoki.org`,
+  themeColor: `#000`,
+  backgroundColor: `#fff`,
+  pathPrefix: null,
+  logo: path.resolve(__dirname, 'src/images/icon.png'),
+  social: {
+    twitter: `morizotter`,
+    // fbAppId: `966242223397117`,
+  },
+};
